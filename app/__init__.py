a87ed7be2345fa02b4de6ad10593dea1924892ec
Arjun Biyani <arjunbiyani@Arjuns-MacBook-Pro.local>
2019-06-12 23:02:05 +0530

    Tasks

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-celery = Celery('reptile',broker=app.config['CELERY_BROKER_URL'])
+celery = Celery('reptile', broker=app.config['CELERY_BROKER_URL'])
 #celery.conf.update(app.config)
 @celery.task
 def my_background_task(arg1, arg2):
